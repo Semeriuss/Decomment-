a80cc7f6505e07fcdaff7e0f4eb8932c94ae7f05
betziii <56472671+betziii@users.noreply.github.com>
2021-06-03 23:24:49 +0300

    Update decomment.c

--- decomment.c
+++ decomment.c
@@ -5,7 +5,7 @@
 /*--------------------------*/
 /* Abenezer Sileshi     ATR */
 /* Aymen Mohammednur    ATR */
-/* Bethelihem Teshibelay ATR*/
+/* Bethelhem Teshibelay ETR/9292/11*/
 /* Semere Habtu ATR/2583/11 */
 /* Semere Tereffe ATR       */
 /*--------------------------*/
